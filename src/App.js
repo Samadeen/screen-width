4c081a133bc1bd5452a27d1a9e0d81ae80d4c475
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-10 05:22:35 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+import WindowTracker from './components/WindowTracker'
 
 const App = () => {
+    const [show, setShow] = useState(false)
+
+    const toggleHandler = () => {
+        setShow(prevShow => !prevShow)
+    }
+
   return (
-    <div>App</div>
+    <main className="container flex">
+        <div className="row flex">
+        <h1>Hello There 😎</h1>
+        <button className='btn' onClick={toggleHandler}>
+            {show === false ? 'Show' : 'Hide'}
+        </button>
+        </div>
+        {show === false ? '' : <WindowTracker/>}
+    </main>
   )
 }
 
